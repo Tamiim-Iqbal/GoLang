3e86bc8a227b15885a48a53bf12dddf7f30df7df
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-01-15 20:46:51 +0600

    Internal Memory

--- 09. Internal Memory/main.go
+++ 09. Internal Memory/main.go
@@ -2,15 +2,28 @@ package main
 import "fmt"
 
 var a = 10
+const p = 11
 
 func add(x int, y int) {
 	z := x + y
 	fmt.Println(z)
 }
 
+func call(){
+	sum := func(x int, y int) {
+		z := x + y
+		fmt.Println(z)
+	}
+	sum(5, 6)
+	sum(p, a)
+}
+
 func main() {
 	add (5, 4)
 	add(a, 3)
+
+	call()
+	fmt.Println(p)
 }
 
 func init() {
